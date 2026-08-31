62e32c751b63ae303de9715c30c862a144857dce
chen <33097588+SF-CHEN@users.noreply.github.com>
2026-08-31 09:16:49 +0800

    refactor: simplify form item wrapper

--- src/common/components/CustomForm/components/FormItemWrapper.vue
+++ src/common/components/CustomForm/components/FormItemWrapper.vue
@@ -1,11 +1,8 @@
 <script lang="ts" setup>
 import type { FormOptionsMap, FormSchemaItem } from "../types"
-import { computed } from "vue"
-import { pickFormItemProps } from "../composables/useFormRules"
-import { hasFormType } from "../registry"
 import FormField from "./FormField.vue"
 
-const props = withDefaults(defineProps<{
+withDefaults(defineProps<{
   item: FormSchemaItem
   form: Record<string, unknown>
   options?: FormOptionsMap
@@ -16,19 +13,17 @@ const props = withDefaults(defineProps<{
 defineEmits<{
   fieldChange: [prop: string, value: unknown]
 }>()
-
-const hasRegisteredType = computed(() => hasFormType(props.item.type || "input"))
 </script>
 
 <template>
-  <el-form-item v-bind="pickFormItemProps(item)" :label-width="item.labelWidth ?? undefined">
+  <el-form-item :prop="item.prop" :label="item.label" :label-width="item.labelWidth">
     <slot :item="item" :form="form" :value="form[item.prop]">
       <FormField
-        v-if="item.type !== 'custom' && hasRegisteredType"
+        v-if="item.type !== 'custom'"
         :item="item"
         :form="form"
         :options="options"
-        @field-change="(prop, val) => $emit('fieldChange', prop, val)"
+        @field-change="(prop, value) => $emit('fieldChange', prop, value)"
       />
     </slot>
   </el-form-item>
