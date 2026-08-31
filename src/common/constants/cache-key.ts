27f70ca1ffb845fd7c39d8768f408585020ea3b9
chen <33097588+SF-CHEN@users.noreply.github.com>
2026-08-31 08:36:18 +0800

    refactor: remove unused layout cache key

--- src/common/constants/cache-key.ts
+++ src/common/constants/cache-key.ts
@@ -2,7 +2,6 @@ const SYSTEM_NAME = "vue-admin-template"
 
 export const CacheKey = {
   TOKEN: `${SYSTEM_NAME}-token-key`,
-  CONFIG_LAYOUT: `${SYSTEM_NAME}-config-layout-key`,
   SIDEBAR_STATUS: `${SYSTEM_NAME}-sidebar-status-key`,
   VISITED_VIEWS: `${SYSTEM_NAME}-visited-views-key`,
   CACHED_VIEWS: `${SYSTEM_NAME}-cached-views-key`
